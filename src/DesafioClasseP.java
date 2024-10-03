ab875efdc150037db437b1d11b89f696013df266
Giovanni Morais <giovannimo1@yahoo.com>
2024-10-02 21:12:01 -0300

    Inital Commit

---
+++ src/DesafioClasseP.java
@@ -0,0 +1,77 @@
+import br.com.alura.screenmatch.modelos.DesafiosClasse2;
+
+public class DesafioClasseP {
+    public static void main(String[] args) {
+        /*DesafiosClasse2 conta = new DesafiosClasse2();
+
+        conta.setNumeroConta(123);
+        conta.setSaldo(1000);
+        conta.titular = "Giovanni";
+
+        System.out.println("Número da Conta: " + conta.getNumeroConta());
+        System.out.println("Saldo: " + conta.getSaldo());
+        System.out.println("Titular: " + conta.titular);
+
+        conta.setSaldo(1500);
+        System.out.println("Novo saldo: " + conta.getSaldo());*/
+
+
+        //Desafio 2 - Idade
+        /*DesafiosClasse2 pessoa1 = new DesafiosClasse2();
+        pessoa1.setIdade(22);
+        pessoa1.setNome("Luís");
+
+        DesafiosClasse2 pessoa2 = new DesafiosClasse2();
+        pessoa2.setNome("Rodrigo");
+        pessoa2.setIdade(20);
+
+        System.out.println(pessoa1.getNome() + " tem " + pessoa1.getIdade() + " anos.");
+        pessoa1.verificarIdade();
+
+        System.out.println(pessoa2.getNome() + " tem " + pessoa2.getIdade() + " anos.");
+        pessoa2.verificarIdade();*/
+
+
+        //Desafio 3 - Produto
+        /*DesafiosClasse2 produto = new DesafiosClasse2("Celular", 2000.0);
+
+        System.out.println("Nome do Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getPreco());
+
+        produto.aplicarDesconto(10);
+        System.out.println("Novo Preço após Desconto: " + produto.getPreco());*/
+
+
+        //Desafio 4 - Aluno
+        /*DesafiosClasse2 aluno1 = new DesafiosClasse2("Lulu", 7.5, 8.0, 9.2);
+        DesafiosClasse2 aluno2 = new DesafiosClasse2("Karina", 6.8, 7.3, 8.5);
+
+        System.out.println("Aluno 1:");
+        System.out.println("Nome: " + aluno1.getNome());
+        System.out.println("Nota 1: " + aluno1.getNota1());
+        System.out.println("Nota 2: " + aluno1.getNota2());
+        System.out.println("Nota 3: " + aluno1.getNota3());
+        System.out.println("Média: " + aluno1.calcularMedia());
+        System.out.println();
+
+        System.out.println("Aluno 2:");
+        System.out.println("Nome: " + aluno2.getNome());
+        System.out.println("Nota 1: " + aluno2.getNota1());
+        System.out.println("Nota 2: " + aluno2.getNota2());
+        System.out.println("Nota 3: " + aluno2.getNota3());
+        System.out.println("Média: " + aluno2.calcularMedia());*/
+
+
+        //Desafio 5 - Livro
+        /*DesafiosClasse2 livro1 = new DesafiosClasse2();
+        livro1.setTitulo("Lógica de Programação");
+        livro1.setAutor("Paulo Silveira");
+
+        DesafiosClasse2 livro2 = new DesafiosClasse2();
+        livro2.setTitulo("A lógica do jogo");
+        livro2.setAutor("Marcus Becker");
+
+        livro1.exibirDetalhes();
+        livro2.exibirDetalhes();*/
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/DesafiosClasse2.java
@@ -0,0 +1,160 @@
+package br.com.alura.screenmatch.modelos;
+
+//Desafio 1 - Conta bancaria
+public class DesafiosClasse2 {
+    /*private int numeroConta;
+    private double saldo;
+    public String titular;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }*/
+
+
+    //Desafio 2 - Idade
+    /*private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void verificarIdade() {
+        if (idade >= 18) {
+            System.out.println(nome + " é maior de idade.");
+        } else {
+            System.out.println(nome + " não é maior de idade.");
+        }
+    }*/
+
+
+    //Desafio 3 - Produto
+    /*private String nome;
+    private double preco;
+
+    public DesafiosClasse2(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void aplicarDesconto(double percentual) {
+        double desconto = preco * (percentual / 100);
+        preco -= desconto;
+    }*/
+
+
+    //Desafio 4 - Aluno
+    /*private String nome;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+    public DesafiosClasse2(String nome, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3) / 3;
+    }*/
+
+
+    //Desafio 5 - Livro
+    /*private String titulo;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+    }*/
+}
